aad6fb08163d5e65e2f8f1d560826fa2cfad4b38
F4isalSH <f4isalcode@gmail.com>
2022-04-05 10:25:06 +0300

    Removing import

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,5 +1,4 @@
 import { errorHandler } from "./error-handler.middleware";
 import { notFound } from "./not-found.middleware";
-import { authCheck } from "./auth-check.middleware";
 
-export { errorHandler, notFound, authCheck };
+export { errorHandler, notFound };
