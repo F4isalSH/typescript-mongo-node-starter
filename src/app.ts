948de8f9f2d072d17492852a30483844191417b7
F4isalSH <f4isalcode@gmail.com>
2022-04-06 11:53:46 +0300

    Added ConnectDB function

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application, Request, Response } from "express";
 import "express-async-errors";
 import { errorHandler, notFound } from "./middlewares";
+import { connectDB } from "./database/connect";
+require("dotenv").config();
 
 const app: Application = express();
 const port: number | string = process.env.PORT || 3000;
+const url: string = process.env.MONGO_URI!;
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
@@ -15,6 +18,8 @@ app.use(notFound);
 
 const start = async () => {
   try {
+    await connectDB(url);
+    console.log("Connect to DB...");
     app.listen(port, () => console.log(`Server running on port ${port}`));
   } catch (err: any) {
     console.log(err);
